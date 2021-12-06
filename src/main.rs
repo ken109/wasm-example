81ac6e42ceda9b5b24d853e45db5f3b119ff12ad
Kensuke Kubo <kensukekubo19@gmail.com>
2021-12-06 23:51:19 +0900

    fix: use trunk

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+mod pages;
+
+fn main() {
+    yew::start_app::<pages::Model>();
+}
--- src/pages/mod.rs
+++ src/pages/mod.rs
@@ -1,1 +1,29 @@
 pub(crate) mod top;
+
+use yew::prelude::*;
+
+#[derive(Debug)]
+pub struct Model;
+
+impl Component for Model {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        Self
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        unimplemented!()
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <top::Top />
+        }
+    }
+}
